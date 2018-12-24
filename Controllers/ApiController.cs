e378c212cb99c54a3fd5e6040996293bea874c50
Raül Torralba <raul.torralba@gmail.com>
2018-12-24 11:30:40 +0100

    Initial commit

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nrs;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace app.Controllers
+{
+    [Route("api/[controller]")]
+    public class NrsController : Controller
+    {
+        // POST api/nrs
+        [HttpPost]
+        [Consumes("application/x-www-form-urlencoded")]
+        public Nrs.NrsData Post([FromForm]Nrs.NrsData nrsData)
+        {
+            return nrsData;
+        }
+    }
+}
---
+++ NrsData.cs
@@ -0,0 +1,11 @@
+namespace Nrs {
+    public class NrsData
+    {
+        public string Id { get; set; }
+        public string Channel { get; set; }
+        public string Event { get; set; }
+        public string ContactId { get; set; }
+        public string CampaignId { get; set; }
+        public string CampaignName { get; set; }
+    }
+}
